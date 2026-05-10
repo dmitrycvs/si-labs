c037136bed7d058b690b9ead60e6c46d2d96ec66
scott <cvasiuc.dima@gmail.com>
2026-05-10 21:17:14 +0300

    added lab 6.1

--- src/config.h
+++ src/config.h
@@ -1,43 +1,8 @@
 #pragma once
-#include <Arduino.h>
-#include "tasks/task_sensor.h"
-#include "tasks/task_driver.h"
-#include "tasks/task_monitor.h"
+#include <stdint.h>
 
-namespace Config
-{
-  constexpr uint8_t DHT_PIN      = 3;
-  constexpr uint8_t FAN_PIN      = 5;   // PWM-capable pin
-  constexpr uint8_t BTN_UP_PIN   = 9;
-  constexpr uint8_t BTN_DOWN_PIN = 8;
-
-  static constexpr TaskSensor::Config SENSOR_CFG{
-      .dhtPin        = DHT_PIN,
-      .upPin         = BTN_UP_PIN,
-      .downPin       = BTN_DOWN_PIN,
-      .activeLow     = true,
-      .debounceMs    = 50,
-      .targetInit    = 50,
-      .targetMin     = 20,
-      .targetMax     = 80,
-      .targetStep    = 1,
-      .kp            = 3.0f,
-      .ki            = 0.05f,
-      .kd            = 0.5f,
-      .outMin        = 0.0f,
-      .outMax        = 100.0f,
-      .btnPeriodMs   = 50,
-      .measurePeriodMs = 2000,
-      .pidPeriodMs   = 2000,
-  };
-
-  static constexpr TaskDriver::Config DRIVER_CFG{
-      .fanPin    = FAN_PIN,
-      .fanMinPwm = 30,
-      .periodMs  = 50,
-  };
-
-  static constexpr TaskMonitor::Config MONITOR_CFG{
-      .periodMs = 2000,
-  };
+namespace Config {
+  static constexpr uint8_t  PIN_LED     = 13;
+  static constexpr uint8_t  PIN_BUTTON  = 2;
+  static constexpr uint16_t DEBOUNCE_MS = 50;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,38 @@
 #include <Arduino.h>
 #include "Console.h"
+#include "PushButton.h"
 #include "config.h"
-#include "tasks/task_sensor.h"
-#include "tasks/task_driver.h"
-#include "tasks/task_monitor.h"
 
-void setup()
-{
+enum class LedState { OFF, ON };
+
+static LedState   g_state = LedState::OFF;
+static PushButton g_button;
+
+void setup() {
   Console::init();
 
-  TaskSensor::setup(Config::SENSOR_CFG);
-  TaskDriver::setup(Config::DRIVER_CFG);
-  TaskMonitor::setup(Config::MONITOR_CFG);
+  pinMode(Config::PIN_LED, OUTPUT);
+  digitalWrite(Config::PIN_LED, LOW);
+
+  g_button.setup({ Config::PIN_BUTTON, true, Config::DEBOUNCE_MS });
+
+  printf("ButtonLED FSM ready\r\n");
+  printf("State: OFF\r\n");
 }
 
-void loop()
-{
-  TaskSensor::tick();
-  const TaskSensor::State s = TaskSensor::getState();
+void loop() {
+  if (!g_button.pollRisingEdge()) return;
 
-  TaskDriver::commandDutyPct(s.pwmDuty);
-  TaskDriver::tick();
-
-  TaskMonitor::tick(s, TaskDriver::isActive());
+  switch (g_state) {
+    case LedState::OFF:
+      g_state = LedState::ON;
+      digitalWrite(Config::PIN_LED, HIGH);
+      printf("State: ON\r\n");
+      break;
+    case LedState::ON:
+      g_state = LedState::OFF;
+      digitalWrite(Config::PIN_LED, LOW);
+      printf("State: OFF\r\n");
+      break;
+  }
 }
